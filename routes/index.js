f3c3cb191cfa7fd832e0e3bb53a2fa44b03b8ccc
Eiwen <boldiy@sina.com>
2024-05-17 13:37:28 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const router = require('koa-router')()
 const { generateIndex, generateLogin } = require("../middleware/generate-html")
 
-router.get('/login', generateLogin)
+router.get('/login', generateLogin) //this is a remark
+
+//this is a new line for test
+
 router.get('/', generateIndex)
 
 module.exports = router
